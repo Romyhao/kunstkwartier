4985095df0f3cc2c2db16252f2c7ee7fd024fbfa
Sagaana <165004002+SagaanaSivasundaram@users.noreply.github.com>
2025-11-25 14:27:01 +0100

    Pagina zover (navigatie moet nog afgemaakt worden)

--- kunstkwartier/frontend/app.js
+++ kunstkwartier/frontend/app.js
@@ -1,1 +1,8 @@
-console.log("JavaScript file linked successfully");
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');       // verandert hamburger in X
+    offScreenMenu.classList.toggle('menu');   // schuift menu in/uit
+});
+
