0a10e122012e8519081b075cd8b9116106836c07
Sagaana <165004002+SagaanaSivasundaram@users.noreply.github.com>
2025-11-26 14:40:22 +0100

    start up begin

--- kunstkwartier/frontend/app.js
+++ kunstkwartier/frontend/app.js
@@ -5,4 +5,3 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');       // verandert hamburger in X
     offScreenMenu.classList.toggle('menu');   // schuift menu in/uit
 });
-
