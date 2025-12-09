5a718162ecb6befee30d3c9200502073ace699f0
Sagaana <165004002+SagaanaSivasundaram@users.noreply.github.com>
2025-12-09 14:21:24 +0100

    kleine verandering home2

--- kunstkwartier/frontend/app.js
+++ kunstkwartier/frontend/app.js
@@ -2,6 +2,18 @@ const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
 hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');       // verandert hamburger in X
+    hamMenu.classList.toggle('active');       // X ding
     offScreenMenu.classList.toggle('menu');   // schuift menu in/uit
+});
+
+
+// Home2 zoekbalk code voor het enter klikken via keyboard in plaats van de button//
+
+const zoekInput = document.getElementById("zoekInput");
+
+zoekInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        e.preventDefault();   
+        handleSearchPosts(zoekInput.value);  
+    }
 });
